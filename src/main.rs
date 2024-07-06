5d6dd9317c739b45f63b0302e496fececfaa18ce
satler-git <mitsclatw24@mbr.nifty.com>
2024-07-06 08:45:00 +0000

    Add web support

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,10 @@
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
+#[cfg(not(target_arch = "wasm32"))]
 use std::time::Instant;
-
-fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-    eframe::run_native("Rend", options, Box::new(|_cc| Box::<MyApp>::default()))
-}
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 
 struct MyApp {
     pushed: i64,
@@ -28,6 +22,14 @@ impl Default for MyApp {
             started: false,
             start_time: None,
         }
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl MyApp {
+    /// Called once before the first frame.
+    pub fn new() -> Self {
+        Default::default()
     }
 }
 
@@ -81,4 +83,50 @@ impl eframe::App for MyApp {
             }
         });
     }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native("Rend", options, Box::new(|_cc| Box::<MyApp>::default()))
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "the_canvas_id",
+                web_options,
+                Box::new(|_cc| Box::new(MyApp::new())),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
